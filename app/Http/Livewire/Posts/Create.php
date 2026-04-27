96f37d08c6ab2f3782269838b4fc972d81fa2fb5
tri3yul <yuliantotri851@gmail.com>
2026-04-27 09:00:29 +0700

    feat: update create.php

--- app/Http/Livewire/Posts/Create.php
+++ app/Http/Livewire/Posts/Create.php
@@ -24,7 +24,7 @@ class Create extends Component
     public function updatedFile()
     {
         $this->validate([
-            'file' => 'mimes:' . implode(',', array_merge($this->imageFormats, $this->videoFormats)) . '|max:2048',
+            'file' => 'mimes:' . implode(',', array_merge($this->imageFormats, $this->videoFormats)) . '|max:4096',
         ]);
     }
 
@@ -33,7 +33,7 @@ class Create extends Component
         $data = $this->validate([
             'title' => 'required|max:50',
             'body' => 'required|max:1000',
-            'file' => 'nullable|mimes:' . implode(',', array_merge($this->imageFormats, $this->videoFormats)) . '|max:2048',
+            'file' => 'nullable|mimes:' . implode(',', array_merge($this->imageFormats, $this->videoFormats)) . '|max:4096',
         ]);
 
         $post = Post::create([
